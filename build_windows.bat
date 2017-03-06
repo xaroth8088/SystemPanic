bd70e0819e4ebeffff32e57308d4ad78695ad9a3
xaroth <xaroth@gmail.com>
2017-03-05 17:45:59 -0800

    Fixes windows build script

--- build_windows.bat
+++ build_windows.bat
@@ -2,3 +2,6 @@ rmdir /s /q build
 rmdir /s /q dist
 pyinstaller --onefile --windowed --name=SystemPanic SystemPanic\__main__.py
 xcopy /s SystemPanic\GamePaks dist\GamePaks\*
+xcopy /s SystemPanic\FX dist\FX\*
+mkdir dist\Core
+copy SystemPanic\Core\*.ttf dist\Core
